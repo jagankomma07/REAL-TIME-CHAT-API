141802a72fad3d796e7c06d661188fe202e68851
jagan komma <jagankomma@jagans-MacBook-Air.local>
2026-01-30 15:49:27 +0530

    first commit

---
+++ app/main.py
@@ -0,0 +1,8 @@
+from fastapi import FastAPI
+from app.routes import auth_routes, chat_routes, websocket_routes
+
+app = FastAPI()
+
+app.include_router(auth_routes.router)
+app.include_router(chat_routes.router)
+app.include_router(websocket_routes.router)
